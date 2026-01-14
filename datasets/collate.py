30c751b3e5be86297378733b368ec8bbd8354e2c
shubhangishubhangiphd2023-coder <shubhangi.shubhangi.phd2023@sitpune.edu.in>
2026-01-15 02:07:59 +0530

    Create collate.py

---
+++ datasets/collate.py
@@ -0,0 +1,37 @@
+def mosei_collate_fn(batch):
+"""
+Pads variable-length sequences for MulT
+"""
+
+
+# -------- TEXT --------
+input_ids = [b['text']['input_ids'] for b in batch]
+attention_mask = [b['text']['attention_mask'] for b in batch]
+
+
+input_ids = pad_sequence(input_ids, batch_first=True, padding_value=1)
+attention_mask = pad_sequence(attention_mask, batch_first=True, padding_value=0)
+
+
+# -------- AUDIO / VISION --------
+audio = pad_sequence([b['audio'] for b in batch], batch_first=True)
+vision = pad_sequence([b['vision'] for b in batch], batch_first=True)
+
+
+# -------- LABELS --------
+emotion = torch.stack([b['emotion'] for b in batch])
+sentiment = torch.stack([b['sentiment'] for b in batch])
+
+
+return {
+'inputs': {
+'text': {
+'input_ids': input_ids,
+'attention_mask': attention_mask
+},
+'audio': audio,
+'vision': vision
+},
+'emotion': emotion,
+'sentiment': sentiment
+}
